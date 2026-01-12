a18828aa5224f5f3107262999a10a385ec35a59a
Minh Phan <20395809@k12.hi.us>
2026-01-12 13:03:59 -1000

    remove dotfile

--- backend/UserManagement/obj/Debug/net9.0/UserManagement.AssemblyInfo.cs
+++ backend/UserManagement/obj/Debug/net9.0/UserManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06d6c38dab4c282c947552ae17c07f6f2b7b5b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01587be141b197682f65478326cc9e7343535db7")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
